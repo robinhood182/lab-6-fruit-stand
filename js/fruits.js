f34ff83d0f64f13d6a4cd25afe685fb5ef159dec
Robyn Navarro <rnavarro182@gmail.com>
2018-05-08 14:54:59 -0700

    html and fruit objects

---
+++ js/fruits.js
@@ -0,0 +1,34 @@
+/*exported fruits */
+'use strict';
+
+var fruits = [
+    {
+        mango: {
+            taste: 'sweet',
+            color: 'orange',
+            insideColor: 'yellow',
+            price: 3
+            createLi: function() {
+                const = 
+            }
+        },
+        avocado: {
+            taste: 'neutral',
+            color: 'green',
+            insideColor: 'yellow',
+            price: 2
+        },
+        pepper: {
+            taste: 'savory-spicy',
+            color: 'red',
+            insideColor: 'red',
+            price: 1
+        },
+        guanabana: {
+            taste: 'sour-sweet',
+            color: 'green',
+            insideColor: 'white',
+            price: 5
+        }
+    }
+];
