8996c71fc235e6fb1849aa8bdb619f1f82743eff
Robyn Navarro <rnavarro182@gmail.com>
2018-05-08 17:06:22 -0700

    progress on displaying fruits

--- js/app.js
+++ js/app.js
@@ -1,3 +1,18 @@
 /* globals fruits */
 'use strict';
 
+function insertFruits() {
+    var fruitDisplay = document.getElementById('fruit-list');
+    for(var i = 0; i < fruits.length; i++) {
+        console.log(fruits[i].createLi());
+        var fruitList = fruits[i].createLi();
+        console.log(fruitList);
+        fruitDisplay.appendChild(fruitList);
+    }
+
+}
+
+function insertImage() {
+    
+}
+insertFruits();
--- js/fruits.js
+++ js/fruits.js
@@ -2,33 +2,60 @@
 'use strict';
 
 var fruits = [
+
     {
-        mango: {
-            taste: 'sweet',
-            color: 'orange',
-            insideColor: 'yellow',
-            price: 3
-            createLi: function() {
-                const = 
-            }
-        },
-        avocado: {
-            taste: 'neutral',
-            color: 'green',
-            insideColor: 'yellow',
-            price: 2
-        },
-        pepper: {
-            taste: 'savory-spicy',
-            color: 'red',
-            insideColor: 'red',
-            price: 1
-        },
-        guanabana: {
-            taste: 'sour-sweet',
-            color: 'green',
-            insideColor: 'white',
-            price: 5
+        name: 'Mango',
+        taste: 'sweet',
+        color: 'orange',
+        insideColor: 'yellow',
+        price: 3,
+        image: 'images/mango.jpg',
+        createLi: function() {
+            const li = document.createElement('li');
+            li.textContent = this.name + this.color;
+            return li;
+        }
+    },
+
+    {
+        name: 'Avocado',
+        taste: 'neutral',
+        color: 'green',
+        insideColor: 'yellow',
+        price: 2,
+        image: 'images/avocado.jpg',
+        createLi: function() {
+            const li = document.createElement('li');
+            li.textContent = this.name + this.color;
+            return li;
+        }
+    },
+
+    {
+        name: 'Pepper',
+        taste: 'savory-spicy',
+        color: 'red',
+        insideColor: 'red',
+        price: 1,
+        image: 'images/pepper.jpg',
+        createLi: function() {
+            const li = document.createElement('li');
+            li.textContent = this.name + this.color;
+            return li;
+        }
+    },
+
+    {
+        name: 'Guanabana',
+        taste: 'sour-sweet',
+        color: 'green',
+        insideColor: 'white',
+        price: 5,
+        image: 'images/guanabana.jpg',
+        createLi: function() {
+            const li = document.createElement('li');
+            li.textContent = this.name + this.color;
+            return li;
         }
     }
 ];
